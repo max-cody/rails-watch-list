6df7a9ab45a736e9597a979272fad1e3ccc27448
max cody <Max.cody1@outlook.com>
2024-05-02 18:26:13 +0100

    all gereen baby that bookmark controller can get in the bin

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,37 +1,75 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
+# class BookmarksController < ApplicationController
+#   before_action :set_movie, only: %i[new create]
+
+#   # def index
+#   #   @bookmarks = Bookmark.all
+#   # end
+
+#   def new
+#     @bookmark = Bookmark.new
+#     @list = List.find(params[:list_id])
+#   end
+
+#   def show
+#     @bookmark = Bookmark.find(params[:id])
+#   end
+
+#   def create
+#     @bookmark = Bookmark.new(bookmark_params)
+#     @list = List.find(params[:list_id])
+#     @bookmark.list = @list
+
+#     if @bookmark.save
+#       redirect_to @bookmark, notice: 'bookmark successfully created.'
+#     else
+#       render :lists/show
+#     end
+#   end
+#  private
+#   def destroy
+#     @bookmark = Bookmark.find(params[:id])
+#     @bookmark.destroy
+#     redirect_to movies_new_path(@bookmark.movie), status: :see_other
+#   end
+
+#   private
+
+#   def bookmark_params
+#     params.require(:bookmark).permit(:comment, :movie_id)
+#   end
+
+#   def set_movie
+#     @movie = Movie.find_by(id: params[:movie_id])
+#     return if @movie
+
+#     # Handle the case where the movie doesn't exist
+#     # For example, redirect back with a flash message
+#     redirect_to movies_new_path, alert: 'Movie not found'
+#   end
+# end
 class BookmarksController < ApplicationController
-  before_action :set_movie, only: %i[new create]
-
-  def index
-    @bookmarks = Bookmark.all
-  end
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
-  end
-
-  def show
-    @bookmark = Bookmark.find(params[:id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
     @bookmark.list = @list
-
     if @bookmark.save
-      redirect_to @bookmark, notice: 'bookmark successfully created.'
+      redirect_to list_path(@list)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
- private
+
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to movies_new_path(@bookmark.movie), status: :see_other
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
@@ -40,12 +78,11 @@ class BookmarksController < ApplicationController
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 
-  def set_movie
-    @movie = Movie.find_by(id: params[:movie_id])
-    return if @movie
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 
-    # Handle the case where the movie doesn't exist
-    # For example, redirect back with a flash message
-    redirect_to movies_new_path, alert: 'Movie not found'
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
